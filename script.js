af1a85cbb0b26282d5ab8fe5366bd558a3b52431
ericleirosario <ericleirosario@gmail.com>
2023-10-19 22:13:00 -0300

    implementação de funcionalidades

---
+++ script.js
@@ -0,0 +1,37 @@
+const result = document.querySelector(".result");
+const humanScore = document.querySelector("#human-score");
+const machineScore = document.querySelector("#machine-score");
+
+let humanScoreNumber = 0;
+let machineScoreNumber = 0;
+
+console.log(machineScore)
+
+const playHuman = (humanChoise) => {
+  playTheGame(humanChoise, playMachine());
+};
+
+const playMachine = () => {
+  const choices = ["rock", "paper", "scissors"];
+  const randomNumber = Math.floor(Math.random() * 3);
+
+  return choices[randomNumber];
+};
+
+const playTheGame = (human, machine) => {
+  if (human === machine) {
+    result.innerHTML = "Empate!";
+  } else if (
+    (human === "rock" && machine === "scissors") ||
+    (human === "paper" && machine === "rock") ||
+    (human === "scissors" && machine === "paper")
+  ) {
+    humanScoreNumber++
+    humanScore.innerHTML = humanScoreNumber;
+    result.innerHTML = "Você Venceu!";
+  } else {
+    machineScoreNumber++
+    machineScore.innerHTML = machineScoreNumber;
+    result.innerHTML = "Você Perdeu!";
+  }
+};
